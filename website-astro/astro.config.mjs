7eea90f4733aba8f5747b68434c94124d968e1cc
faqwa <faqwa@users.noreply.github.com>
2025-09-27 09:19:22 +1000

    Configure Astro build for GitHub Pages

--- website-astro/astro.config.mjs
+++ website-astro/astro.config.mjs
@@ -1,8 +1,9 @@
-﻿import { defineConfig } from 'astro/config';
+import { defineConfig } from 'astro/config';
 import mdx from '@astrojs/mdx';
 import react from '@astrojs/react';
 
 export default defineConfig({
+  site: 'https://faqwa.github.io/arc-',
+  base: '/arc-',
   integrations: [mdx(), react()],
-  site: 'https://arkup.xbyali.page',
 });
